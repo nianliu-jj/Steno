aab114570ac2db4845144c0b07ded4772b13c5b3
nianliu-jj <fire-phenix@foxmail.com>
2026-05-13 15:50:44 +0800

    chore: 忽略 Tauri 构建产物的 lint 检查

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from '@soybeanjs/eslint-config-vue';
 
-export default defineConfig({
-  'vue/component-name-in-template-casing': [
-    'warn',
-    'PascalCase',
-    {
-      registeredComponentsOnly: false,
-      ignores: ['/^icon-/']
-    }
-  ]
-});
+export default [
+  { ignores: ['dist/**', 'src-tauri/target/**', 'src-tauri/gen/**'] },
+  ...(await defineConfig({
+    'vue/component-name-in-template-casing': [
+      'warn',
+      'PascalCase',
+      {
+        registeredComponentsOnly: false,
+        ignores: ['/^icon-/']
+      }
+    ]
+  }))
+];
